a9b330ca50eb654a6e05cfd6131b29a7f3ea9ede
eduardofg87 <goncalves.ef@gmail.com>
2020-05-01 03:24:41 -0300

    adicionado cardfooter eduardofg87

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -23,6 +23,7 @@ import {
 } from './styles';
 
 import avatar from '../../images/avatar.png';
+import avatar2 from '../../images/pirocoptero-avatar.jpg';
 
 export default function Activities() {
     return (
@@ -52,6 +53,43 @@ export default function Activities() {
 
                         <Feather name="lock" color="#fff" size={14} />
                         <Date>Há 2 anos</Date>
+                    </Details>
+
+                    <Actions>
+                        <Option>
+                            <MaterialCommunityIcons name="comment-outline" size={14} color="#fff" />
+                            <OptionLabel>0</OptionLabel>
+                        </Option>
+
+                        <Option>
+                            <AntDesign name="hearto" size={14} color="#fff" />
+                            <OptionLabel>0</OptionLabel>
+                        </Option>
+                    </Actions>
+                </CardFooter>
+            </Card>
+
+            <Card>
+                <CardHeader>
+                    <Avatar source={avatar2} />
+                    <Description>
+                        <Bold>Você</Bold> pagou a <Bold>@eduardofg87</Bold>
+                    </Description>
+                </CardHeader>
+
+                <CardBody>
+                    <UserName>Eduardo</UserName>
+                </CardBody>
+
+                <CardFooter>
+                    <Details>
+                        <Value>R$ 1.000,00</Value>
+
+                        <Divider/>
+
+
+                        <Feather name="lock" color="#fff" size={14} />
+                        <Date>Há 5 minutos</Date>
                     </Details>
 
                     <Actions>
